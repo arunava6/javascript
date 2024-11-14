c186623eb774df621e69294619dc5b24261b3bb4
Simplilearn_Github <siddam.bharat@simplilearn.net>
2024-11-14 20:23:31 +0530

    commit

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,15 @@
+let name="Arunava"
+let marks=30
+console.log(typeof(name));
+console.log(typeof(marks));
+
+
+// datatype conversion
+let score="33"
+console.log(typeof(score));
+
+let convert=Number(score)       // here, we change string to number
+console.log(typeof(convert))
+
+
+
---
+++ 01_basics/var.js
@@ -0,0 +1,11 @@
+const accountId=14453
+let accountEmail="arunavasaha@gmail.com"
+var accountPass="@11122"   // we should avoid use of var
+accountCity="Siliguri"
+let accountState
+
+console.log(accountId);
+// Used to print the values in tabular form
+console.table([accountId,accountEmail,accountPass,accountCity,accountState])
+
+
