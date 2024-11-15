fd0fc3238719368161751e2793a1021fc0979d6e
Simplilearn_Github <siddam.bharat@simplilearn.net>
2024-11-15 11:47:14 +0530

    first commit

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -11,5 +11,23 @@ console.log(typeof(score));
 let convert=Number(score)       // here, we change string to number
 console.log(typeof(convert))
 
+// 7 types of primitive datatypes:
+// String, boolean, number,null,undefined,Symbol, BigInt
 
+// Reference(Non-premitive)
+// Array, objects, Function
 
+//Array
+const fruits=["Mango","Apple","Orange"]
+
+//Object
+let obj={
+    name:"Arunava",
+    age:21
+}
+
+// Function
+const myfunc=function(){
+    console.log("hello World!")
+}
+ 
---
+++ 01_basics/string.js
@@ -0,0 +1,8 @@
+const name="Arunava"
+const repoCount=15
+// Use of backtick `` 
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName=new String('Arunava')
+console.log(gameName[1]);  // for accessing 'r'
+console.log(gameName.toUpperCase())     
