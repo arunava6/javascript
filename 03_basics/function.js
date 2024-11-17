e8d3f058583e50d5f4113fb2b882d10aa9d8bec2
Simplilearn_Github <siddam.bharat@simplilearn.net>
2024-11-17 12:45:34 +0530

    first commit

---
+++ 03_basics/function.js
@@ -0,0 +1,18 @@
+function hello(){
+    console.log("Hello, World!");
+}
+hello()
+
+function addNumber(num1,num2){
+    let sum=num1+num2
+    return sum;
+}
+console.log(addNumber(2,3));
+
+
+function greeting(name){
+    console.log(`Good Morning ${name}`);
+}
+greeting("Arunava")
+
+
