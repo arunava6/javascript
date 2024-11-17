9453b46b64d03dd9f9a9eea3327307053609814c
Simplilearn_Github <siddam.bharat@simplilearn.net>
2024-11-17 12:55:44 +0530

    first commit

--- 03_basics/function.js
+++ 03_basics/function.js
@@ -16,3 +16,32 @@ function greeting(name){
 greeting("Arunava")
 
 
+function value(...num1){        // ...num1 is used to fetch other values from parameter
+    return num1;
+}
+console.log(value(1,2,3,4));
+
+
+// function in objects
+const obj1={
+    name:"Arunava",
+    age:21
+}
+
+function result(getobject){
+    console.log(`My name is ${getobject.name} and my age is ${getobject.age}`);
+    
+}
+result(obj1)       
+
+
+// Function in array
+const arr1=[1,2,3,4]
+
+function arr_func(getarray){
+    console.log(getarray[1]);
+    
+}
+arr_func(arr1)
+
+
